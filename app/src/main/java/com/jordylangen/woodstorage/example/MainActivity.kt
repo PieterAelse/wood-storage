164d0b9d8c5b96de439dfb3e0fa0161a1cc07a26
Pieter Otten <pieter.otten@mediaan.com>
2019-02-21 13:16:06 +0100

    Also converted the app to Kotlin

---
+++ app/src/main/java/com/jordylangen/woodstorage/example/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jordylangen.woodstorage.example
+
+import android.content.Intent
+import android.os.PersistableBundle
+import android.os.Bundle
+import android.view.View
+
+import com.jordylangen.woodstorage.WoodStorageViewActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.d("onCreate")
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button_open_overview)?.setOnClickListener {
+            startActivity(Intent(this@MainActivity, WoodStorageViewActivity::class.java))
+        }
+
+        findViewById<View>(R.id.button_log_exception)?.setOnClickListener {
+            Timber.e(Throwable("Something horrible happened!"),"oh noes, an exception!")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("onResume")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Timber.d("onSaveInstanceState")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("onDestroy")
+    }
+}
---
+++ app/src/main/java/com/jordylangen/woodstorage/example/WoodStorageExampleApplication.kt
@@ -0,0 +1,24 @@
+package com.jordylangen.woodstorage.example
+
+import android.app.Application
+
+import com.jordylangen.woodstorage.WoodStorageFactory
+
+import timber.log.Timber
+
+class WoodStorageExampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+        Timber.plant(WoodStorageFactory.getInstance(this))
+
+        Timber.d("example application started")
+    }
+
+    override fun onTerminate() {
+        Timber.d("example application stopped")
+        super.onTerminate()
+    }
+}
